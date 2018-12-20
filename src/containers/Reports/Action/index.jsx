a17e8c5951ee18b855d66a9752da7960535962e7
alfiant <alfiant@gmail.com>
2018-12-20 11:26:34 +0800

    WIP Reports Action

--- src/containers/Reports/Action/index.jsx
+++ src/containers/Reports/Action/index.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import { Container, Row } from 'reactstrap';
+import VipIssues from './components/VipIssues';
+import WaitingForActionIssues from './components/WaitingForActionIssues';
+import UnassignedDefects from './components/UnassignedDefects';
+import IssuesMissingDetail from './components/IssuesMissingDetail';
 
 const ReportAction = () => (
   <Container className="dashboard">
     <Row>
-      Placeholder
+      <VipIssues />
+      <WaitingForActionIssues />
+      <UnassignedDefects />
+      <IssuesMissingDetail />
     </Row>
   </Container>
 );

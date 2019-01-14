af6e47634431430d3dd97203ee32aedf708386d2
alfiant <alfiant@gmail.com>
2019-01-14 09:41:13 -0800

    Issues By Month WIP

--- src/containers/Reports/Status/index.jsx
+++ src/containers/Reports/Status/index.jsx
@@ -3,12 +3,16 @@ import { Container, Row } from 'reactstrap';
 
 import OpenIssues from './components/OpenIssues';
 import Issues2bCompleted from './components/Issues2bCompleted';
+import IssuesByMonth from './components/IssuesByMonth';
 
 const ReportAction = () => (
   <Container className="dashboard">
     <Row>
       <OpenIssues />
       <Issues2bCompleted />
+    </Row>
+    <Row>
+      <IssuesByMonth />
     </Row>
   </Container>
 );

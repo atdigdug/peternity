9695a0745fcbca56fa457e0c896c862de5074fac
alfiant <alfiant@gmail.com>
2019-01-22 16:12:45 -0800

    Reports-Business-Risk Priorities

--- src/containers/Reports/Business/index.jsx
+++ src/containers/Reports/Business/index.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { Container, Row } from 'reactstrap';
 
 import Findings from './components/Findings';
+import RiskPriorities from './components/RiskPriorities';
 
 const ReportAction = () => (
   <Container className="dashboard">
     <Row>
       <Findings />
+      <RiskPriorities />
     </Row>
   </Container>
 );

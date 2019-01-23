6c3cf29e59bfaa3ebc872f3fd68638c2d1769c09
alfiant <alfiant@gmail.com>
2019-01-22 18:14:32 -0800

    Reports-Ciso remaining panes

--- src/containers/Reports/Ciso/index.jsx
+++ src/containers/Reports/Ciso/index.jsx
@@ -7,6 +7,11 @@ import WindowOfExposure from './components/WindowOfExposure';
 import WindowOfExposure24Months from './components/WindowOfExposure24Months';
 import MissedSlo from './components/MissedSlo';
 import AvgDaysMissedSlo from './components/AvgDaysMissedSlo';
+import SuccessChart from './components/SuccessChart';
+import FailureChart from './components/FailureChart';
+import EngagementsByMonth from './components/EngagementsByMonth';
+import EngagementsByConsultant from './components/EngagementsByConsultant';
+import EngagementsByPhase from './components/EngagementsByPhase';
 
 const ReportAction = () => (
   <Container className="dashboard">
@@ -15,6 +20,15 @@ const ReportAction = () => (
       <WindowOfExposure24Months />
       <MissedSlo />
       <AvgDaysMissedSlo />
+    </Row>
+    <Row>
+      <SuccessChart />
+      <FailureChart />
+    </Row>
+    <Row>
+      <EngagementsByMonth />
+      <EngagementsByConsultant />
+      <EngagementsByPhase />
     </Row>
     <Row>
       <Findings />

47b63e7809607bb68faa7c019d3488c7814ed4c4
alfiant <alfiant@gmail.com>
2019-01-23 16:52:55 -0800

    Home - header

--- src/containers/Dashboards/Default/index.jsx
+++ src/containers/Dashboards/Default/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Row } from 'reactstrap';
+import { Container, Row, Col } from 'reactstrap';
 import NeutralAlertsWithIcons from './components/NeutralAlertsWithIcons';
 import TopSellingProducts from './components/TopSellingProducts';
 import InboxTable from './components/InboxTable';
@@ -7,6 +7,11 @@ import emails from './components/email-list';
 
 const HomePage = () => (
   <Container className="dashboard">
+    <Row>
+      <Col md={12}>
+        <h3 className="page-title">Home</h3>
+      </Col>
+    </Row>
     <Row>
       <NeutralAlertsWithIcons />
     </Row>

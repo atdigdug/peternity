3322afc1fd84822311cb1c7608722179b11b00c9
alfiant <alfiant@gmail.com>
2019-02-04 12:07:28 -0800

    Engagement all fields

--- src/containers/Engagements/Input/components/HorizontalForm.jsx
+++ src/containers/Engagements/Input/components/HorizontalForm.jsx
@@ -6,7 +6,6 @@ import PropTypes from 'prop-types';
 
 class HorizontalForm extends PureComponent {
   static propTypes = {
-    t: PropTypes.func.isRequired,
     handleSubmit: PropTypes.func.isRequired,
     reset: PropTypes.func.isRequired,
   };
@@ -26,15 +25,15 @@ class HorizontalForm extends PureComponent {
   };
 
   render() {
-    const { handleSubmit, reset, t } = this.props;
+    const { handleSubmit, reset } = this.props;
 
     return (
       <Col md={12} lg={12}>
         <Card>
           <CardBody>
             <div className="card__title">
-              <h5 className="bold-text">{t('forms.basic_form.horizontal_form')}</h5>
-              <h5 className="subhead">Labels are left from fields</h5>
+              <h5 className="bold-text">New Engagement</h5>
+              <h5 className="subhead">Enter as many fields as possible</h5>
             </div>
             <form className="form form--horizontal" onSubmit={handleSubmit}>
               <div className="form__form-group">
@@ -138,21 +137,187 @@ class HorizontalForm extends PureComponent {
                 </div>
               </div>
               <div className="form__form-group">
-                <span className="form__form-group-label">
-Project name,
-Project manager,
-Project status,
-Expected to go live date,
-System information owner, created by, last modified by,
-Priority,
-Resolution,
-Assignee,
-Date reported, created, updated,
-Date assigned to AppSec, date acknowledged, date consultant assigned, date request fulfilled,
-CSEQ submitted date,
-Is self-service,
-Comments
-                </span>
+                <span className="form__form-group-label">Project Manager</span>
+                <div className="form__form-group-field">
+                  <Field
+                    name="defaultInput"
+                    component="input"
+                    type="text"
+                  />
+                </div>
+              </div>
+              <div className="form__form-group">
+                <span className="form__form-group-label">Project Status</span>
+                <div className="form__form-group-field">
+                  <Field
+                    name="defaultInput"
+                    component="input"
+                    type="text"
+                  />
+                </div>
+              </div>
+              <div className="form__form-group">
+                <span className="form__form-group-label">Expected to go live date</span>
+                <div className="form__form-group-field">
+                  <Field
+                    name="defaultInput"
+                    component="input"
+                    type="text"
+                  />
+                </div>
+              </div>
+              <div className="form__form-group">
+                <span className="form__form-group-label">System Information Owner</span>
+                <div className="form__form-group-field">
+                  <Field
+                    name="defaultInput"
+                    component="input"
+                    type="text"
+                  />
+                </div>
+              </div>
+              <div className="form__form-group">
+                <span className="form__form-group-label">Created By</span>
+                <div className="form__form-group-field">
+                  <Field
+                    name="defaultInput"
+                    component="input"
+                    type="text"
+                  />
+                </div>
+              </div>
+              <div className="form__form-group">
+                <span className="form__form-group-label">Last Modified By</span>
+                <div className="form__form-group-field">
+                  <Field
+                    name="defaultInput"
+                    component="input"
+                    type="text"
+                  />
+                </div>
+              </div>
+              <div className="form__form-group">
+                <span className="form__form-group-label">Priority</span>
+                <div className="form__form-group-field">
+                  <Field
+                    name="defaultInput"
+                    component="input"
+                    type="text"
+                  />
+                </div>
+              </div>
+              <div className="form__form-group">
+                <span className="form__form-group-label">Resolution</span>
+                <div className="form__form-group-field">
+                  <Field
+                    name="defaultInput"
+                    component="input"
+                    type="text"
+                  />
+                </div>
+              </div>
+              <div className="form__form-group">
+                <span className="form__form-group-label">Assignee</span>
+                <div className="form__form-group-field">
+                  <Field
+                    name="defaultInput"
+                    component="input"
+                    type="text"
+                  />
+                </div>
+              </div>
+              <div className="form__form-group">
+                <span className="form__form-group-label">Date Reported</span>
+                <div className="form__form-group-field">
+                  <Field
+                    name="defaultInput"
+                    component="input"
+                    type="text"
+                  />
+                </div>
+              </div>
+              <div className="form__form-group">
+                <span className="form__form-group-label">Date Created</span>
+                <div className="form__form-group-field">
+                  <Field
+                    name="defaultInput"
+                    component="input"
+                    type="text"
+                  />
+                </div>
+              </div>
+              <div className="form__form-group">
+                <span className="form__form-group-label">Date Updated</span>
+                <div className="form__form-group-field">
+                  <Field
+                    name="defaultInput"
+                    component="input"
+                    type="text"
+                  />
+                </div>
+              </div>
+              <div className="form__form-group">
+                <span className="form__form-group-label">Date assigned to AppSec</span>
+                <div className="form__form-group-field">
+                  <Field
+                    name="defaultInput"
+                    component="input"
+                    type="text"
+                  />
+                </div>
+              </div>
+              <div className="form__form-group">
+                <span className="form__form-group-label">Date Acknowledged</span>
+                <div className="form__form-group-field">
+                  <Field
+                    name="defaultInput"
+                    component="input"
+                    type="text"
+                  />
+                </div>
+              </div>
+              <div className="form__form-group">
+                <span className="form__form-group-label">Date Consultant Assigned</span>
+                <div className="form__form-group-field">
+                  <Field
+                    name="defaultInput"
+                    component="input"
+                    type="text"
+                  />
+                </div>
+              </div>
+              <div className="form__form-group">
+                <span className="form__form-group-label">Date Request Fulfilled</span>
+                <div className="form__form-group-field">
+                  <Field
+                    name="defaultInput"
+                    component="input"
+                    type="text"
+                  />
+                </div>
+              </div>
+              <div className="form__form-group">
+                <span className="form__form-group-label">CSEQ Submitted Date</span>
+                <div className="form__form-group-field">
+                  <Field
+                    name="defaultInput"
+                    component="input"
+                    type="text"
+                  />
+                </div>
+              </div>
+              <div className="form__form-group">
+                <span className="form__form-group-label">Is Self-Service</span>
+                <div className="form__form-group-field">
+                  <Field
+                    name="defaultInput"
+                    component="input"
+                    type="text"
+                  />
+                </div>
+              </div>
+              <div className="form__form-group">
+                <span className="form__form-group-label">Comments</span>
                 <div className="form__form-group-field">
                   <Field
                     name="defaultInput"

71191d36b3f44e3101789c29cfd67ced452a4227
alfiant <alfiant@gmail.com>
2019-02-06 15:00:44 -0800

    Findings - duplicate chat framework

--- src/containers/Findings/index.jsx
+++ src/containers/Findings/index.jsx
@@ -1,19 +1,24 @@
 import React from 'react';
-import { Container, Row, Col } from 'reactstrap';
-import InboxTable from './components/InboxTable';
-import emails from './components/email-list';
+import { Col, Container, Row } from 'reactstrap';
+import { translate } from 'react-i18next';
+import PropTypes from 'prop-types';
+import ChatCard from './components/ChatCard';
 
-const HomePage = () => (
-  <Container className="dashboard">
+const Chat = ({ t }) => (
+  <Container>
     <Row>
       <Col md={12}>
-        <h3 className="page-title">Findings</h3>
+        <h3 className="page-title">{t('chat_application.page_title')}</h3>
       </Col>
     </Row>
     <Row>
-      <InboxTable emails={emails} />
+      <ChatCard />
     </Row>
   </Container>
 );
 
-export default HomePage;
+Chat.propTypes = {
+  t: PropTypes.func.isRequired,
+};
+
+export default translate('common')(Chat);

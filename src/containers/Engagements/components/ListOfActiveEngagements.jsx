bc1a57c09495d086d8ab2a423fbe6bb97b8684ce
alfiant <alfiant@gmail.com>
2019-02-20 11:57:32 -0800

    Engagement Landing: tweak list of engagements

--- src/containers/Engagements/components/ListOfActiveEngagements.jsx
+++ src/containers/Engagements/components/ListOfActiveEngagements.jsx
@@ -1,44 +1,48 @@
 import React from 'react';
 import { Card, CardBody, Row, Col } from 'reactstrap';
-import Panel from '../../../shared/components/Panel';
 
 const ListOfActiveEngagements = () => (
-  <Panel xl={12}>
-    <Row>
-      <Col xl={3}>
-        <Card>
-          <CardBody style={{ backgroundColor: '#90ee90' }}>
-            <h1 className="bold-text">E1</h1>
-            Engagement 1
-          </CardBody>
-        </Card>
-      </Col>
-      <Col xl={3}>
-        <Card>
-          <CardBody style={{ backgroundColor: '#90ee90' }}>
-            <h1 className="bold-text">E2</h1>
-            Engagement 2
-          </CardBody>
-        </Card>
-      </Col>
-      <Col xl={3}>
-        <Card>
-          <CardBody style={{ backgroundColor: '#90ee90' }}>
-            <h1 className="bold-text">E3</h1>
-            Engagement 3
-          </CardBody>
-        </Card>
-      </Col>
-      <Col xl={3}>
-        <Card>
-          <CardBody style={{ backgroundColor: '#90ee90' }}>
-            <h1 className="bold-text">E4</h1>
-            Engagement 4
-          </CardBody>
-        </Card>
-      </Col>
-    </Row>
-  </Panel>
+  <Col xl={12}>
+    <Card>
+      <CardBody>
+        <div style={{ textAlign: 'right' }}>Recent | Favorites</div><p />
+        <Row>
+          <Col xl={3}>
+            <Card>
+              <CardBody style={{ backgroundColor: '#add8e6' }}>
+                <h1 className="bold-text">E1</h1>
+                Engagement 1
+              </CardBody>
+            </Card>
+          </Col>
+          <Col xl={3}>
+            <Card>
+              <CardBody style={{ backgroundColor: '#add8e6' }}>
+                <h1 className="bold-text">E2</h1>
+                Engagement 2
+              </CardBody>
+            </Card>
+          </Col>
+          <Col xl={3}>
+            <Card>
+              <CardBody style={{ backgroundColor: '#add8e6' }}>
+                <h1 className="bold-text">E3</h1>
+                Engagement 3
+              </CardBody>
+            </Card>
+          </Col>
+          <Col xl={3}>
+            <Card>
+              <CardBody style={{ backgroundColor: '#add8e6' }}>
+                <h1 className="bold-text">E4</h1>
+                Engagement 4
+              </CardBody>
+            </Card>
+          </Col>
+        </Row>
+      </CardBody>
+    </Card>
+  </Col>
 );
 
 export default ListOfActiveEngagements;

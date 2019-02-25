bb2bc19bec171145e0b9b465a4d3219417f4fd89
alfiant <alfiant@gmail.com>
2019-02-24 18:03:55 -0800

    Dev support - messages tweak

--- src/containers/DevSupport/components/DataTable.jsx
+++ src/containers/DevSupport/components/DataTable.jsx
@@ -15,11 +15,13 @@ export default class DataTable extends PureComponent {
         key: 'first',
         name: 'From',
         sortable: true,
+        width: 100,
       },
       {
         key: 'user',
         name: 'Context',
         sortable: true,
+        width: 200,
       },
       {
         key: 'age',
@@ -48,9 +50,9 @@ export default class DataTable extends PureComponent {
         id: i,
         first: ['Alice', 'Bob', 'Chandra'][Math.floor((Math.random() * 3))],
         user: ['E1 / App1 / Scan1', 'E1 / App2 / Scan2', 'E2 / App1 / Scan 4'][Math.floor((Math.random() * 3))],
-        age: ['Message 1. Arrived compass prepare an on as. Reasonable particular on my it in sympathize.',
-          'Message 2. Assure polite his really and others figure though. Day age advantages end sufficient eat.',
-          'Message 3. Say ferrars demands besides her address. Blind going you merit few fancy their.',
+        age: ['Message 1. Arrived compass prepare an on as. Reasonable particular on my it in sympathize ...',
+          'Message 2. Assure polite his really and others figure though. Day age advantages end sufficient eat ...',
+          'Message 3. Say ferrars demands besides her address. Blind going you merit few fancy their ...',
         ][Math.floor((Math.random() * 3))],
       });
     }

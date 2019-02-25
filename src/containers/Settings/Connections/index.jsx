3c58974a704891e1777c57a82762d3cc03b7864f
alfiant <alfiant@gmail.com>
2019-02-24 18:24:13 -0800

    Settings connections - clean up

--- src/containers/Settings/Connections/index.jsx
+++ src/containers/Settings/Connections/index.jsx
@@ -9,64 +9,13 @@ const ScansDetail = () => (
       </Col>
     </Row>
     <Row>
-      <Col>
-        <Card>
-          <CardBody>
-            <form className="form">
-              <div className="form__form-group">
-                <Row>
-                  <Col xl="4">
-                    <div className="form__form-group-field">
-                      <input />
-                    </div>
-                  </Col>
-                  <Col xl="2">
-                    <Button color="primary" type="submit">Search</Button>
-                  </Col>
-                </Row>
-              </div>
-            </form>
-          </CardBody>
-        </Card>
-      </Col>
+      <Col><Button color="primary" type="submit">Add Connection</Button></Col>
     </Row>
     <Row>
       <Col>
         <Card>
           <CardBody>
-            <div className="card__title">
-              <h5 className="bold-text">Knowledge Base</h5>
-            </div>
-            <form className="form">
-              <div className="form__form-group">
-                <Row>
-                  <Col xl="6">
-                    <div className="form__form-group-field">
-                      <input />
-                    </div>
-                  </Col>
-                  <Col xl="6">
-                    <Button color="primary" type="submit">Search</Button>
-                  </Col>
-                </Row>
-              </div>
-            </form>
-            <h5 className="bold-text">Bookmarks</h5>
-            Link 1 | Link 2 | Link 3
-            <p />
-            <h5 className="bold-text">Most Recent</h5>
-            Link 1 | Link 2 | Link 3
-          </CardBody>
-        </Card>
-      </Col>
-      <Col>
-        <Card>
-          <CardBody>
-            <div className="card__title">
-              <h5 className="bold-text">Metrics</h5>
-            </div>
-            Time spent: 13 hours 5 minutes.
-            <p />
+            List of connections
           </CardBody>
         </Card>
       </Col>

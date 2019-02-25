c2e881923796d1acc13c57365ff067f1c28ea6bd
alfiant <alfiant@gmail.com>
2019-02-24 16:04:54 -0800

    Finding detail - action buttons

--- src/containers/Findings/Detail/index.jsx
+++ src/containers/Findings/Detail/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Col, Container, Row, Card, CardBody } from 'reactstrap';
+import { Col, Container, Row, Card, CardBody, Button } from 'reactstrap';
 import { Breadcrumb } from 'react-bootstrap';
 import HorizontalForm from './components/HorizontalForm';
 
@@ -7,8 +7,8 @@ const ScansDetail = () => (
   <Container>
     <Row>
       <Col md={12}>
-        <h3 className="page-title">WhiteHat Scan Details</h3>
-        <h3 className="page-subhead subhead">WhiteHat scan of Sonia App</h3>
+        <h3 className="page-title">Finding 1234</h3>
+        <h3 className="page-subhead subhead">From WhiteHat scan of Sonia App</h3>
       </Col>
     </Row>
     <Row>
@@ -16,8 +16,24 @@ const ScansDetail = () => (
         <Breadcrumb>
           <Breadcrumb.Item href="/engagements/detail">Annual Sale Launch Engagement</Breadcrumb.Item>
           <Breadcrumb.Item href="/appsmodules/detail">Sonia App</Breadcrumb.Item>
-          <Breadcrumb.Item active>WhiteHat Scan</Breadcrumb.Item>
+          <Breadcrumb.Item href="/scans/detail">WhiteHat Scan</Breadcrumb.Item>
+          <Breadcrumb.Item active>Finding 1234</Breadcrumb.Item>
         </Breadcrumb>
+      </Col>
+    </Row>
+    <Row>
+      <Col xl="12">
+        <Card>
+          <CardBody>
+            <form className="form">
+              <div className="form__form-group">
+                <Button color="primary" type="submit">Action 1</Button>
+                <Button color="primary" type="submit">Action 2</Button>
+                <Button color="primary" type="submit">Action 3</Button>
+              </div>
+            </form>
+          </CardBody>
+        </Card>
       </Col>
     </Row>
     <Row>

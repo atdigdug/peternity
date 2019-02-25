ec0f64b515a79305461d116a372e01b616c9d7f2
alfiant <alfiant@gmail.com>
2019-02-24 18:52:18 -0800

    Settings connections final look

--- src/containers/Settings/Connections/Add/index.jsx
+++ src/containers/Settings/Connections/Add/index.jsx
@@ -1,6 +1,15 @@
 import React from 'react';
 import { Col, Container, Row, Card, CardBody } from 'reactstrap';
 
+const logoWhitehat = `${process.env.PUBLIC_URL}/img/whitehat.png`;
+const logoVeracode = `${process.env.PUBLIC_URL}/img/veracode.png`;
+
+const logostyle = {
+  left: 0,
+  width: 100,
+  lineHeight: '24px',
+};
+
 const ScansDetail = () => (
   <Container>
     <Row>
@@ -14,6 +23,74 @@ const ScansDetail = () => (
           <CardBody>
             <div className="card__title">
               <h5 className="bold-text">Platform</h5>
+            </div>
+          </CardBody>
+        </Card>
+      </Col>
+    </Row>
+    <Row>
+      <Col>
+        <Card>
+          <CardBody>
+            <div className="card__title">
+              <h5 className="bold-text">Scanners</h5>
+            </div>
+            <img style={logostyle} src={logoWhitehat} alt="" />
+            <img style={logostyle} src={logoVeracode} alt="" />
+          </CardBody>
+        </Card>
+      </Col>
+    </Row>
+    <Row>
+      <Col>
+        <Card>
+          <CardBody>
+            <div className="card__title">
+              <h5 className="bold-text">CI/CD</h5>
+            </div>
+          </CardBody>
+        </Card>
+      </Col>
+    </Row>
+    <Row>
+      <Col>
+        <Card>
+          <CardBody>
+            <div className="card__title">
+              <h5 className="bold-text">Developer Tools</h5>
+            </div>
+          </CardBody>
+        </Card>
+      </Col>
+    </Row>
+    <Row>
+      <Col>
+        <Card>
+          <CardBody>
+            <div className="card__title">
+              <h5 className="bold-text">Communication</h5>
+            </div>
+          </CardBody>
+        </Card>
+      </Col>
+    </Row>
+    <Row>
+      <Col>
+        <Card>
+          <CardBody>
+            <div className="card__title">
+              <h5 className="bold-text">CMDB</h5>
+            </div>
+          </CardBody>
+        </Card>
+      </Col>
+    </Row>
+    <Row>
+      <Col>
+        <Card>
+          <CardBody>
+            <div className="card__title">
+              <h5 className="bold-text">GRC</h5>
             </div>
           </CardBody>
         </Card>
--- src/containers/Settings/Connections/index.jsx
+++ src/containers/Settings/Connections/index.jsx
@@ -2,6 +2,15 @@ import React from 'react';
 import { Col, Container, Row, Card, CardBody, Button } from 'reactstrap';
 import { NavLink } from 'react-router-dom';
 
+const logoWhitehat = `${process.env.PUBLIC_URL}/img/whitehat.png`;
+const logoVeracode = `${process.env.PUBLIC_URL}/img/veracode.png`;
+
+const logostyle = {
+  left: 0,
+  width: 100,
+  lineHeight: '24px',
+};
+
 const ScansDetail = () => (
   <Container>
     <Row>
@@ -20,7 +29,8 @@ const ScansDetail = () => (
       <Col>
         <Card>
           <CardBody>
-            List of connections
+            <img style={logostyle} src={logoWhitehat} alt="" />
+            <img style={logostyle} src={logoVeracode} alt="" />
           </CardBody>
         </Card>
       </Col>

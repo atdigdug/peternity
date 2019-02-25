bd3c9ae5bb983145d4c7fc3ae6b43f45ff01344c
alfiant <alfiant@gmail.com>
2019-02-24 16:30:53 -0800

    Finding detail - conversation

--- src/containers/Findings/Detail/index.jsx
+++ src/containers/Findings/Detail/index.jsx
@@ -43,11 +43,11 @@ const ScansDetail = () => (
             <div className="card__title">
               <h5 className="bold-text">Description</h5>
             </div>
-              System displays an error message Eg: Unable to update the record.
-              But according to functionality system should update the updated record.
-              This particular defect occurs in version 9.2.
-              This particular defect also found in other modules.
-              Inconsistency of application while reproducing the defect.
+            System displays an error message Eg: Unable to update the record.
+            But according to functionality system should update the updated record.
+            This particular defect occurs in version 9.2.
+            This particular defect also found in other modules.
+            Inconsistency of application while reproducing the defect.
           </CardBody>
         </Card>
       </Col>
@@ -60,6 +60,24 @@ const ScansDetail = () => (
             <Row>
               <Col><HorizontalForm /></Col>
             </Row>
+          </CardBody>
+        </Card>
+      </Col>
+    </Row>
+    <Row>
+      <Col>
+        <Card>
+          <CardBody>
+            <div className="card__title">
+              <h5 className="bold-text">Conversation</h5>
+            </div>
+            Mom: Hi hon, how was your day?<p />
+            Tasha: Fine.<p />
+            Mom: Have you started on your homework yet?<p />
+            Tasha: Sort of…but this show is almost over….<p />
+            Mom: OK, you can finish the show, but you really need to get started as soon as the show is over.
+            I don’t want you staying up until midnight like you did last night.<p />
+            Tasha: Fine.
           </CardBody>
         </Card>
       </Col>

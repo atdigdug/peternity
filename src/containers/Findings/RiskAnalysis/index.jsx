ed2aad6886b3b711ddbe189274f7e02179a74a44
alfiant <alfiant@gmail.com>
2019-03-12 10:23:13 -0700

    risk analysis - adding detail

--- src/containers/Findings/RiskAnalysis/index.jsx
+++ src/containers/Findings/RiskAnalysis/index.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
-import { Col, Container, Row, Button } from 'reactstrap';
+import { Col, Container, Row, Button, Card, CardBody } from 'reactstrap';
+import { NavLink } from 'react-router-dom';
 import Table from './components/DataTable';
 import RiskFilter from './components/RiskFilter';
 import RuleFilter from './components/RuleFilter';
+import HorizontalForm from './components/HorizontalForm';
 
 const ApplicationsDefault = () => (
   <Container>
@@ -21,6 +23,47 @@ const ApplicationsDefault = () => (
     <Row>
       <Col xl="3"><Row><RiskFilter /></Row><Row><RuleFilter /></Row></Col>
       <Col xl="9"><Table /></Col>
+    </Row>
+    <Row>
+      <Col xl="12">
+        <Card>
+          <CardBody>
+            <NavLink to="/findings/detail">
+              <div className="card__title">
+                <h5 className="bold-text">Finding 1234 / Scan 1 / App 1</h5>
+              </div>
+            </NavLink>
+            <Row>
+              <Col>
+                <Card>
+                  <CardBody>
+                    <div className="card__title">
+                      <h5 className="bold-text">Description</h5>
+                    </div>
+                    System displays an error message Eg: Unable to update the record.
+                    But according to functionality system should update the updated record.
+                    This particular defect occurs in version 9.2.
+                    This particular defect also found in other modules.
+                    Inconsistency of application while reproducing the defect.
+                  </CardBody>
+                </Card>
+              </Col>
+              <Col>
+                <Card>
+                  <CardBody>
+                    <div className="card__title">
+                      <h5 className="bold-text">Details</h5>
+                    </div>
+                    <Row>
+                      <Col><HorizontalForm /></Col>
+                    </Row>
+                  </CardBody>
+                </Card>
+              </Col>
+            </Row>
+          </CardBody>
+        </Card>
+      </Col>
     </Row>
   </Container>
 );

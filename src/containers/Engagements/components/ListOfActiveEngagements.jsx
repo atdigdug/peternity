ad16d530eb8848676693b51a6ddf25f23a1c74cb
alfiant <alfiant@gmail.com>
2019-03-14 15:34:57 -0700

    Engagement names

--- src/containers/Engagements/components/ListOfActiveEngagements.jsx
+++ src/containers/Engagements/components/ListOfActiveEngagements.jsx
@@ -12,8 +12,8 @@ const ListOfActiveEngagements = () => (
             <NavLink to="/engagements/detail">
               <Card>
                 <CardBody style={{ backgroundColor: '#add8e6' }}>
-                  <h1 className="bold-text">E1</h1>
-                  Engagement 1
+                  <h1 className="bold-text">Merger Release</h1>
+                  Merger Release
                 </CardBody>
               </Card>
             </NavLink>
@@ -21,24 +21,24 @@ const ListOfActiveEngagements = () => (
           <Col xl={3}>
             <Card>
               <CardBody style={{ backgroundColor: '#add8e6' }}>
-                <h1 className="bold-text">E2</h1>
-                Engagement 2
+                <h1 className="bold-text">Spring Release</h1>
+                Spring Release
               </CardBody>
             </Card>
           </Col>
           <Col xl={3}>
             <Card>
               <CardBody style={{ backgroundColor: '#add8e6' }}>
-                <h1 className="bold-text">E3</h1>
-                Engagement 3
+                <h1 className="bold-text">CRM Update</h1>
+                CRM Update
               </CardBody>
             </Card>
           </Col>
           <Col xl={3}>
             <Card>
               <CardBody style={{ backgroundColor: '#add8e6' }}>
-                <h1 className="bold-text">E4</h1>
-                Engagement 4
+                <h1 className="bold-text">China 2020</h1>
+                China 2020
               </CardBody>
             </Card>
           </Col>

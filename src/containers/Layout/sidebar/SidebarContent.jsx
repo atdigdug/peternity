057b502bf35d0fffcd57d7db4406ad48e417056b
alfiant <alfiant@gmail.com>
2019-03-20 15:57:09 -0700

    nav icons

--- src/containers/Layout/sidebar/SidebarContent.jsx
+++ src/containers/Layout/sidebar/SidebarContent.jsx
@@ -21,7 +21,7 @@ class SidebarContent extends Component {
       <div className="sidebar__content">
         <ul className="sidebar__block">
           <SidebarLink title="Home" icon="home" route="/dashboard_default" onClick={this.hideSidebar} />
-          <SidebarCategory title="Reports" icon="chart-bars">
+          <SidebarCategory title="Reports" icon="pie-chart">
             <SidebarLink title="Action" route="/reports/action" onClick={this.hideSidebar} />
             <SidebarLink title="Status" route="/reports/status" onClick={this.hideSidebar} />
             <SidebarLink title="Business" route="/reports/business" onClick={this.hideSidebar} />
@@ -29,15 +29,15 @@ class SidebarContent extends Component {
             <SidebarLink title="Dev Team" route="/reports/devteam" onClick={this.hideSidebar} />
             <SidebarLink title="External" route="/reports/external" onClick={this.hideSidebar} />
           </SidebarCategory>
-          <SidebarLink title="Engagements" icon="mustache" route="/engagements" onClick={this.hideSidebar} />
-          <SidebarLink title="Apps/Modules" icon="mustache" route="/appsmodules" onClick={this.hideSidebar} />
-          <SidebarLink title="Scans" icon="mustache" route="/scans" onClick={this.hideSidebar} />
-          <SidebarLink title="Findings" icon="mustache" route="/findings" onClick={this.hideSidebar} />
-          <SidebarLink title="Risks" icon="mustache" route="/findings/riskanalysis" onClick={this.hideSidebar} />
-          <SidebarLink title="Dev Support" icon="mustache" route="/devsupport" onClick={this.hideSidebar} />
+          <SidebarLink title="Engagements" icon="arrow-up-circle" route="/engagements" onClick={this.hideSidebar} />
+          <SidebarLink title="Apps/Modules" icon="layers" route="/appsmodules" onClick={this.hideSidebar} />
+          <SidebarLink title="Scans" icon="magnifier" route="/scans" onClick={this.hideSidebar} />
+          <SidebarLink title="Findings" icon="bug" route="/findings" onClick={this.hideSidebar} />
+          <SidebarLink title="Risks" icon="warning" route="/findings/riskanalysis" onClick={this.hideSidebar} />
+          <SidebarLink title="Dev Support" icon="code" route="/devsupport" onClick={this.hideSidebar} />
         </ul>
         <ul className="sidebar__block">
-          <SidebarCategory title="Settings" icon="layers">
+          <SidebarCategory title="Settings" icon="cog">
             <SidebarLink title="Connections" route="/settings/connections" onClick={this.hideSidebar} />
             <button className="sidebar__link" type="button" onClick={changeToLight}>
               <p className="sidebar__link-title">Light Theme</p>

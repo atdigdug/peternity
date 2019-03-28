834195bea6c7e687cfb8db811d1cdf4f5db80e8c
alfiant <alfiant@gmail.com>
2019-03-28 15:19:38 -0700

    engagement - toggle view framework

--- src/containers/Engagements/index.jsx
+++ src/containers/Engagements/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import {
   Col, Container, Row, UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem, ButtonGroup, Button,
 } from 'reactstrap';
@@ -8,53 +8,73 @@ import CondensedIcon from 'mdi-react/MenuIcon';
 import DetailedIcon from 'mdi-react/ViewAgendaIcon';
 import EngagementsAggregate from './components/EngagementsAggregate';
 import Table from './components/NewDataTable';
+import DetailedTable from './components/DetailedDataTable';
 
-const EngagementDefault = () => (
-  <Container>
-    <Row>
-      <Col>
-        <h3 className="page-title">Engagements</h3>
-        <h3 className="page-subhead subhead">
-          Active 54 &nbsp;&nbsp;&nbsp; Closed 392 &nbsp;&nbsp;&nbsp; Favorites Only 13
-        </h3>
-      </Col>
-      <Col>
-        <div className="float-right">
-          <NavLink to="/engagements/input">
-            <Button color="success">+ New Engagement</Button>
-          </NavLink>
-        </div>
-      </Col>
-    </Row>
-    <Row>
-      <EngagementsAggregate />
-    </Row>
-    <Row>
-      <Col xl="6">
-        <UncontrolledDropdown>
-          <DropdownToggle className="icon icon--right">
-            <p>Most urgent <ChevronDownIcon /></p>
-          </DropdownToggle>
-          <DropdownMenu className="dropdown__menu">
-            <DropdownItem>Most urgent</DropdownItem>
-          </DropdownMenu>
-        </UncontrolledDropdown>
-      </Col>
-      <Col xl="6">
-        <div className="float-right">
-          <ButtonGroup>
-            <Button className="icon"><p><CondensedIcon /> Condensed</p></Button>
-            <Button className="icon" outline><p><DetailedIcon /> Detailed</p></Button>
-          </ButtonGroup>
-        </div>
-      </Col>
-    </Row>
-    <Row>
-      <Col xl="12">
-        <Table />
-      </Col>
-    </Row>
-  </Container>
-);
+export default class EngagementDefault extends PureComponent {
+  constructor() {
+    super();
+    this.state = { isCondensed: true };
+  }
 
-export default EngagementDefault;
+  render() {
+    let dataTable;
+
+    if (this.state.isCondensed) {
+      dataTable = <Table />;
+    } else {
+      dataTable = <DetailedTable />;
+    }
+
+    return (
+      <Container>
+        <Row>
+          <Col>
+            <h3 className="page-title">Engagements</h3>
+            <h3 className="page-subhead subhead">
+              Active 54 &nbsp;&nbsp;&nbsp; Closed 392 &nbsp;&nbsp;&nbsp; Favorites Only 13
+            </h3>
+          </Col>
+          <Col>
+            <div className="float-right">
+              <NavLink to="/engagements/input">
+                <Button color="success">+ New Engagement</Button>
+              </NavLink>
+            </div>
+          </Col>
+        </Row>
+        <Row>
+          <EngagementsAggregate />
+        </Row>
+        <Row>
+          <Col xl="6">
+            <UncontrolledDropdown>
+              <DropdownToggle className="icon icon--right">
+                <p>Most urgent <ChevronDownIcon /></p>
+              </DropdownToggle>
+              <DropdownMenu className="dropdown__menu">
+                <DropdownItem>Most urgent</DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+          </Col>
+          <Col xl="6">
+            <div className="float-right">
+              <ButtonGroup>
+                <Button className="icon" onClick={() => this.setState({ isCondensed: true })}>
+                  <p><CondensedIcon /> Condensed</p>
+                </Button>
+                <Button className="icon" outline onClick={() => this.setState({ isCondensed: false })}>
+                  <p><DetailedIcon /> Detailed</p>
+                </Button>
+              </ButtonGroup>
+            </div>
+          </Col>
+        </Row>
+        <Row>
+          <Col xl="12">
+            {dataTable}
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+}

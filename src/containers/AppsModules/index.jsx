a28d005a9fbff42db62aeb0f747270e58a0905b6
alfiant <alfiant@gmail.com>
2019-04-08 14:55:09 -0700

    Apps landing initial

--- src/containers/AppsModules/index.jsx
+++ src/containers/AppsModules/index.jsx
@@ -1,27 +1,79 @@
-import React from 'react';
-import { Col, Container, Row } from 'reactstrap';
-import ListOfActiveEngagements from './components/ListOfActiveEngagements';
+import React, { PureComponent } from 'react';
+import {
+  Col, Container, Row, UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem, ButtonGroup, Button,
+} from 'reactstrap';
+import { NavLink } from 'react-router-dom';
+import ChevronDownIcon from 'mdi-react/ChevronDownIcon';
+import CondensedIcon from 'mdi-react/MenuIcon';
+import DetailedIcon from 'mdi-react/ViewAgendaIcon';
 import EngagementsAggregate from './components/EngagementsAggregate';
 import Table from './components/DataTable';
 
-const ApplicationsDefault = () => (
-  <Container>
-    <Row>
-      <Col md={12}>
-        <h3 className="page-title">Applications and Modules</h3>
-        <h3 className="page-subhead subhead">Dashboard</h3>
-      </Col>
-    </Row>
-    <Row>
-      <ListOfActiveEngagements />
-    </Row>
-    <Row>
-      <EngagementsAggregate />
-    </Row>
-    <Row>
-      <Table />
-    </Row>
-  </Container>
-);
+export default class EngagementDefault extends PureComponent {
+  constructor() {
+    super();
+    this.state = { isCondensed: true };
+  }
 
-export default ApplicationsDefault;
+  render() {
+    let dataTable;
+
+    if (this.state.isCondensed) {
+      dataTable = <Table />;
+    } else {
+      dataTable = <Table />;
+    }
+
+    return (
+      <Container>
+        <Row>
+          <Col>
+            <h3 className="page-title">Engagements</h3>
+            <h3 className="page-subhead subhead">
+              Active 54 &nbsp;&nbsp;&nbsp; Closed 392 &nbsp;&nbsp;&nbsp; Favorites Only 13
+            </h3>
+          </Col>
+          <Col>
+            <div className="float-right">
+              <NavLink to="/engagements/input">
+                <Button color="success">+ New Engagement</Button>
+              </NavLink>
+            </div>
+          </Col>
+        </Row>
+        <Row>
+          <EngagementsAggregate />
+        </Row>
+        <Row>
+          <Col xl="6">
+            <UncontrolledDropdown>
+              <DropdownToggle className="icon icon--right">
+                <p>Most urgent <ChevronDownIcon /></p>
+              </DropdownToggle>
+              <DropdownMenu className="dropdown__menu">
+                <DropdownItem>Most urgent</DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+          </Col>
+          <Col xl="6">
+            <div className="float-right">
+              <ButtonGroup>
+                <Button className="icon" onClick={() => this.setState({ isCondensed: true })}>
+                  <p><CondensedIcon /> Condensed</p>
+                </Button>
+                <Button className="icon" outline onClick={() => this.setState({ isCondensed: false })}>
+                  <p><DetailedIcon /> Detailed</p>
+                </Button>
+              </ButtonGroup>
+            </div>
+          </Col>
+        </Row>
+        <Row>
+          <Col xl="12">
+            {dataTable}
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+}

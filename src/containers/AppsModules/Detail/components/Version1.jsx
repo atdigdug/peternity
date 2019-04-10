43f72fd4b1f69b9201383d9a6c23633dc885fbc6
alfiant <alfiant@gmail.com>
2019-04-10 11:58:12 -0700

    App detail - version wip

--- src/containers/AppsModules/Detail/components/Version1.jsx
+++ src/containers/AppsModules/Detail/components/Version1.jsx
@@ -1,9 +1,10 @@
 /* eslint-disable react/no-array-index-key */
 import React from 'react';
 import {
-  Row, Col, Card, CardBody, Progress,
+  Row, Col, Card, CardBody, Progress, Button,
 } from 'reactstrap';
 import { NavLink } from 'react-router-dom';
+import StatusIcon from 'mdi-react/RotateRightIcon';
 import Donut from './SummaryDonut';
 
 const Summary = () => (
@@ -11,30 +12,48 @@ const Summary = () => (
     <CardBody>
       <div className="card__title">
         <NavLink to="/appsmodules/detail">
-          <h5 className="bold-text">Version 2.14 (LATEST)</h5>
+          <h5 className="bold-text">Version 2.1</h5>
         </NavLink>
-        <span className="subhead">(2/22/2019)</span>
+        <span className="subhead">(1/23/2019)</span>
       </div>
-      <hr />
       <Row>
         <Col xl="4">
+          <h6 className="bold-text">Risk rating:</h6>
+            Low (893)<p />
           <h6 className="bold-text">Findings risk:</h6>
           <Donut />
         </Col>
         <Col xl="4">
-          <h6 className="bold-text">Due Date:</h6>
-          7/27/2019<p />
-          <h6 className="bold-text">Consultant:</h6>
-          Bob<p />
-          <h6 className="bold-text">Risk rating:</h6>
-          Low (893)<p />
-          <h6 className="bold-text">Progress:</h6>
-          <p />
+          <div className="card__title">
+            <h5 className="bold-text">Veracode Scan</h5>
+            <span className="subhead">SAST</span>
+          </div>
           <div className="progress-wrap progress-wrap--middle progress-wrap--blue">
             <Progress value={80} />
           </div>
+          <Button className="rounded"><StatusIcon /> in progress...</Button>
+          <p />as part of Merger Release
+          <div className="card__title">
+            <h5 className="bold-text">Veracode Scan</h5>
+            <span className="subhead">SAST</span>
+          </div>
+          <div className="progress-wrap progress-wrap--middle progress-wrap--blue">
+            <Progress value={80} />
+          </div>
+          <Button className="rounded"><StatusIcon /> in progress...</Button>
+          <p />as part of Merger Release
         </Col>
-        <Col xl="4">&nbsp;</Col>
+        <Col xl="4">
+          <div className="card__title">
+            <h5 className="bold-text">Veracode Scan</h5>
+            <span className="subhead">SAST</span>
+          </div>
+          <div className="progress-wrap progress-wrap--middle progress-wrap--blue">
+            <Progress value={80} />
+          </div>
+          <Button className="rounded"><StatusIcon /> in progress...</Button>
+          <p />as part of Merger Release
+        </Col>
       </Row>
     </CardBody>
   </Card>

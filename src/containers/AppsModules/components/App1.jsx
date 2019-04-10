7b8c574d9f7856961dc20d49708a0064a48c17cd
alfiant <alfiant@gmail.com>
2019-04-10 11:43:11 -0700

    App landing - app detail

--- src/containers/AppsModules/components/App1.jsx
+++ src/containers/AppsModules/components/App1.jsx
@@ -1,36 +1,48 @@
 /* eslint-disable react/no-array-index-key */
 import React from 'react';
 import {
-  Row, Col, Card, CardBody, Progress,
+  Row, Col, Card, CardBody, Progress, Button,
 } from 'reactstrap';
 import { NavLink } from 'react-router-dom';
+import FavoriteIcon from 'mdi-react/StarIcon';
+import StatusIcon from 'mdi-react/DoneIcon';
 import Donut from '../Detail/components/SummaryDonut';
 
 const Summary = () => (
   <Card>
     <CardBody>
-      <div className="card__title">
-        <NavLink to="/appsmodules/detail">
-          <h5 className="bold-text">Partner Mobile App</h5>
-        </NavLink>
-        <span className="subhead">v19.2</span>
-      </div>
-      <div className="progress-wrap progress-wrap--middle">
-        <Progress value={100} />
-      </div>
       <Row>
         <Col>
-          <h6 className="bold-text">Due Date:</h6>
-          7/27/2019<p />
+          <div className="card__title">
+            <NavLink to="/appsmodules/detail">
+              <h5 className="bold-text">Partner Mobile App</h5>
+            </NavLink>
+            <span className="subhead">v19.2</span>
+          </div>
+        </Col>
+        <Col align="right"><FavoriteIcon /></Col>
+      </Row>
+      <Row>
+        <Col>
+          <div className="progress-wrap progress-wrap--middle">
+            <Progress value={100} />
+          </div>
+        </Col>
+        <Col align="right">
+          <Button className="rounded"><StatusIcon /> Risk Model</Button>
+        </Col>
+      </Row>
+      <hr />
+      <Row>
+        <Col>
+          <h6 className="bold-text">Architecture:</h6>
+          Java<p />
+          <h6 className="bold-text">Total Scans:</h6>
+          31<p />
           <h6 className="bold-text">Consultant:</h6>
           Bob<p />
           <h6 className="bold-text">Risk rating:</h6>
           Low (893)<p />
-          <h6 className="bold-text">Progress:</h6>
-          <p />
-          <div className="progress-wrap progress-wrap--middle progress-wrap--blue">
-            <Progress value={80} />
-          </div>
         </Col>
         <Col>
           <h6 className="bold-text">Findings risk:</h6>

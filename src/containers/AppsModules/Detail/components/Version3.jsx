3ba7cd38df296e4c2ca09263153cb388163af74b
alfiant <alfiant@gmail.com>
2019-04-11 13:44:00 -0700

    Version 3 update

--- src/containers/AppsModules/Detail/components/Version3.jsx
+++ src/containers/AppsModules/Detail/components/Version3.jsx
@@ -4,7 +4,6 @@ import {
   Row, Col, Card, CardBody, Progress, Badge,
 } from 'reactstrap';
 import { NavLink } from 'react-router-dom';
-import InProgressIcon from 'mdi-react/RotateRightIcon';
 import CompletedIcon from 'mdi-react/DoneIcon';
 import Donut from './SummaryDonut';
 
@@ -13,48 +12,31 @@ const Summary = () => (
     <CardBody>
       <div className="card__title">
         <NavLink to="/appsmodules/detail">
-          <h5 className="bold-text">Version 2.1</h5>
+          <h5 className="bold-text">Version 2.06</h5>
         </NavLink>
-        <span className="subhead">(1/23/2019)</span>
+        <span className="subhead">(12/6/2018)</span>
       </div>
       <Row>
         <Col xl="4">
           <h6 className="bold-text">Risk rating:</h6>
-            Low (893)<p />
+            Low (1023)<p />
           <h6 className="bold-text">Findings risk:</h6>
           <Donut />
         </Col>
         <Col xl="4">
           <div className="card__title">
-            <h5 className="bold-text">Veracode Scan</h5>
-            <span className="subhead">SAST</span>
-          </div>
-          <div className="progress-wrap progress-wrap--middle progress-wrap--blue">
-            <Progress value={80} />
-          </div>
-          <p><Badge color="success" pill><InProgressIcon /> in progress...</Badge></p>
-          <p />as part of Merger Release
-
-          <div className="card__title">
-            <h5 className="bold-text">Checkmarx Scan</h5>
-            <span className="subhead">OSS</span>
+            <h5 className="bold-text">WhiteHat Scan</h5>
+            <span className="subhead">DAST</span>
           </div>
           <div className="progress-wrap progress-wrap--middle">
             <Progress value={100} />
           </div>
-          <p><Badge pill><CompletedIcon /> 1/24/2019 4:07 PM</Badge></p>
-          <p />as part of CRM Update
+          <Badge pill><CompletedIcon /> 12/16/2018</Badge>
+          <p />
+          as part of <span className="bold-text" style={{ color: 'green' }}>Merger Release</span>
         </Col>
         <Col xl="4">
-          <div className="card__title">
-            <h5 className="bold-text">Nexus Scan</h5>
-            <span className="subhead">OSS</span>
-          </div>
-          <div className="progress-wrap progress-wrap--middle">
-            <Progress value={100} />
-          </div>
-          <p><Badge color="secondary" pill><CompletedIcon /> 1/25/2019 6:30 PM</Badge></p>
-          <p />as part of CRM Update
+          &nbsp;
         </Col>
       </Row>
     </CardBody>

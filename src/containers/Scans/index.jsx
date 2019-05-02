b747bddfea3e2d6e3c9fac239636f7c8163854f9
alfiant <alfiant@gmail.com>
2019-05-01 19:35:59 -0700

    Scan landing - change header

--- src/containers/Scans/index.jsx
+++ src/containers/Scans/index.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import { Col, Container, Row } from 'reactstrap';
+import {
+  Col, Container, Row, Button,
+} from 'reactstrap';
 import ListOfActiveEngagements from './components/ListOfActiveEngagements';
 import EngagementsAggregate from './components/EngagementsAggregate';
 import Table from './components/DataTable';
@@ -7,9 +9,16 @@ import Table from './components/DataTable';
 const ApplicationsDefault = () => (
   <Container>
     <Row>
-      <Col md={12}>
-        <h3 className="page-title">Scans</h3>
-        <h3 className="page-subhead subhead">Dashboard</h3>
+      <Col>
+        <h3 className="page-title">Scanning tools</h3>
+        <h3 className="page-subhead subhead">
+          5 items
+        </h3>
+      </Col>
+      <Col>
+        <div className="float-right">
+          <Button color="success">+ New tool</Button>
+        </div>
       </Col>
     </Row>
     <Row>

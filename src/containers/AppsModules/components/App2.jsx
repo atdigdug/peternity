ad0d30c0d0016795098434b5af708f17f4ab9ab9
alfiant <alfiant@gmail.com>
2019-05-05 18:09:00 -0700

    App landing - app2

--- src/containers/AppsModules/components/App2.jsx
+++ src/containers/AppsModules/components/App2.jsx
@@ -1,12 +1,12 @@
 /* eslint-disable react/no-array-index-key */
 import React from 'react';
 import {
-  Row, Col, Card, CardBody, Progress, Badge,
+  Row, Col, Card, CardBody, Badge,
 } from 'reactstrap';
-import { NavLink } from 'react-router-dom';
 import FavoriteIcon from 'mdi-react/StarOutlineIcon';
-import StatusIcon from 'mdi-react/RotateRightIcon';
-import Donut from '../Detail/components/SummaryDonut';
+import StatusIcon from 'mdi-react/DoneIcon';
+import InProgressIcon from 'mdi-react/RotateRightIcon';
+import FindingsBar from '../../AsfShared/FindingsBar';
 
 const Summary = () => (
   <Card>
@@ -14,39 +14,31 @@ const Summary = () => (
       <Row>
         <Col>
           <div className="card__title">
-            <NavLink to="/appsmodules/detail">
-              <h5 className="bold-text">Customer Portal</h5>
-            </NavLink>
-            <span className="subhead">latest version: v2.14 (2/22/2019)</span>
+            <h5 className="bold-text">Customer Portal</h5>
+            <span className="subhead">#3741</span>
           </div>
+          <Badge pill><StatusIcon /> Intake</Badge>&nbsp;
+          <Badge pill color="success"><InProgressIcon /> versions engaged</Badge>
         </Col>
         <Col align="right"><FavoriteIcon /></Col>
-      </Row>
-      <Row>
-        <Col>
-          <div className="progress-wrap progress-wrap--middle progress-wrap--blue">
-            <Progress value={60} />
-          </div>
-        </Col>
-        <Col align="right">
-          <Badge color="success" pill><StatusIcon /> scans in progress...</Badge>
-        </Col>
       </Row>
       <hr />
       <Row>
         <Col>
-          <h6 className="bold-text">Architecture:</h6>
-          PHP<p />
-          <h6 className="bold-text">Total Scans:</h6>
-          14<p />
           <h6 className="bold-text">Consultant:</h6>
           Chandra<p />
-          <h6 className="bold-text">Risk rating:</h6>
-          Low (893)<p />
+          <h6 className="bold-text">Platform:</h6>
+          N/A<p />
+          <h6 className="bold-text">Architecture:</h6>
+          PHP<p />
         </Col>
         <Col>
-          <h6 className="bold-text">Findings risk:</h6>
-          <Donut />
+          <h6 className="bold-text">VERSION 2.14 (LATEST)</h6>
+          22 Feb 2019<p />
+          <h6 className="bold-text">Risk rating:</h6>
+          TBD<p />
+          <h6 className="bold-text">Findings: 750</h6>
+          <FindingsBar />
         </Col>
       </Row>
     </CardBody>

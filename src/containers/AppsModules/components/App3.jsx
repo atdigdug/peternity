845c1ccf94c8ef73db156b524145c80a38ee0938
alfiant <alfiant@gmail.com>
2019-05-05 18:13:39 -0700

    app landing - app3

--- src/containers/AppsModules/components/App3.jsx
+++ src/containers/AppsModules/components/App3.jsx
@@ -1,12 +1,10 @@
 /* eslint-disable react/no-array-index-key */
 import React from 'react';
 import {
-  Row, Col, Card, CardBody, Progress, Badge,
+  Row, Col, Card, CardBody, Badge,
 } from 'reactstrap';
-import { NavLink } from 'react-router-dom';
 import FavoriteIcon from 'mdi-react/StarOutlineIcon';
-import StatusIcon from 'mdi-react/DoneIcon';
-import Donut from './EmptyDonut';
+import StatusIcon from 'mdi-react/RotateRightIcon';
 
 const Summary = () => (
   <Card>
@@ -14,39 +12,29 @@ const Summary = () => (
       <Row>
         <Col>
           <div className="card__title">
-            <NavLink to="/appsmodules/detail">
-              <h5 className="bold-text">Vendor Registration App</h5>
-            </NavLink>
-            <span className="subhead">latest version: v1.02 (1/23/2019)</span>
+            <h5 className="bold-text">Vendor Registration App</h5>
+            <span className="subhead">#1337</span>
           </div>
+          <Badge pill color="success"><StatusIcon /> Intake</Badge>
         </Col>
         <Col align="right"><FavoriteIcon /></Col>
-      </Row>
-      <Row>
-        <Col>
-          <div className="progress-wrap progress-wrap--middle">
-            <Progress value={20} />
-          </div>
-        </Col>
-        <Col align="right">
-          <Badge pill><StatusIcon /> Threat Model</Badge>
-        </Col>
       </Row>
       <hr />
       <Row>
         <Col>
-          <h6 className="bold-text">Architecture:</h6>
-          Node.js<p />
-          <h6 className="bold-text">Total Scans:</h6>
-          0<p />
           <h6 className="bold-text">Consultant:</h6>
           Chandra<p />
-          <h6 className="bold-text">Risk rating:</h6>
-          TBD<p />
+          <h6 className="bold-text">Platform:</h6>
+          Windows<p />
+          <h6 className="bold-text">Architecture:</h6>
+          Node.js<p />
         </Col>
         <Col>
-          <h6 className="bold-text">Findings risk:</h6>
-          <Donut />
+          <h6 className="bold-text">VERSION 1.0.2 (LATEST)</h6>
+          23 Jan 2019<p />
+          <h6 className="bold-text">Risk rating:</h6>
+          TBD<p />
+          <h6 className="bold-text">Findings: TBD</h6>
         </Col>
       </Row>
     </CardBody>

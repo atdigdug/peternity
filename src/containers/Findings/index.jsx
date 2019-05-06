870c8d6159daf4753fb42d0a2b9177c3bb5f070f
alfiant <alfiant@gmail.com>
2019-05-05 21:26:01 -0700

    findings landing - rehaul wip

--- src/containers/Findings/index.jsx
+++ src/containers/Findings/index.jsx
@@ -2,10 +2,7 @@ import React from 'react';
 import {
   Col, Container, Row, Card, CardBody, Button,
 } from 'reactstrap';
-import { NavLink } from 'react-router-dom';
-import EngagementsAggregate from './components/EngagementsAggregate';
-import Table from './components/DataTable';
-import HorizontalForm from './components/HorizontalForm';
+import Table from '../AsfShared/FindingsTable';
 
 const ApplicationsDefault = () => (
   <Container>
@@ -38,50 +35,8 @@ const ApplicationsDefault = () => (
       </Col>
     </Row>
     <Row>
-      <EngagementsAggregate />
-    </Row>
-    <Row>
-      <Table />
-    </Row>
-    <Row>
-      <Col xl="12">
-        <Card>
-          <CardBody>
-            <NavLink to="/findings/detail">
-              <div className="card__title">
-                <h5 className="bold-text">Finding 1234 / Scan 1 / App 1</h5>
-              </div>
-            </NavLink>
-            <Row>
-              <Col>
-                <Card>
-                  <CardBody>
-                    <div className="card__title">
-                      <h5 className="bold-text">Description</h5>
-                    </div>
-                    System displays an error message Eg: Unable to update the record.
-                    But according to functionality system should update the updated record.
-                    This particular defect occurs in version 9.2.
-                    This particular defect also found in other modules.
-                    Inconsistency of application while reproducing the defect.
-                  </CardBody>
-                </Card>
-              </Col>
-              <Col>
-                <Card>
-                  <CardBody>
-                    <div className="card__title">
-                      <h5 className="bold-text">Details</h5>
-                    </div>
-                    <Row>
-                      <Col><HorizontalForm /></Col>
-                    </Row>
-                  </CardBody>
-                </Card>
-              </Col>
-            </Row>
-          </CardBody>
-        </Card>
+      <Col>
+        <Table />
       </Col>
     </Row>
   </Container>

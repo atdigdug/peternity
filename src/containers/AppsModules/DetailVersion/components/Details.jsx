d5c3f238c45696cbf0cc9cd47818a02135d4adc2
alfiant <alfiant@gmail.com>
2019-05-06 18:41:30 -0700

    app detail version - details

--- src/containers/AppsModules/DetailVersion/components/Details.jsx
+++ src/containers/AppsModules/DetailVersion/components/Details.jsx
@@ -1,32 +1,37 @@
 /* eslint-disable react/no-array-index-key */
 import React from 'react';
 import {
-  Row, Col, Card, CardBody,
+  Row, Col, Card, CardBody, Button,
 } from 'reactstrap';
-import FavoriteIcon from 'mdi-react/FavoriteIcon';
+import HistoryIcon from 'mdi-react/HistoryIcon';
+import FindingsBar from '../../../AsfShared/FindingsBar';
 
 const Summary = () => (
   <Card>
     <CardBody>
       <Row>
         <Col>
-          <FavoriteIcon /> Add to favorites
+          <div className="card__title">
+            <h5 className="bold-text">Details</h5>
+          </div>
           <hr />
           <p />
-          <h6 className="bold-text">Architecture</h6>
-          PHP
+          <h6 className="bold-text">Risk Rating:</h6>
+          TBD
           <p />
-          <h6 className="bold-text">Total Scans</h6>
-          14
+          <h6 className="bold-text">Findings: 750</h6>
+          <FindingsBar />
           <p />
-          <h6 className="bold-text">Consultant</h6>
-          Chandra
-          <p />
-          <h6 className="bold-text">Description</h6>
+          <h6 className="bold-text">Version Notes:</h6>
           ZenMana and MSAT have announced today the planned merger of their business operations to offer a unified
           mobile and communications solution to the enterprise market. ZenMana will acquire the MSAT assets, people
           and customer contracts and the merged business will operate under the ZenMana brand.
           <p />
+          <Button outline><HistoryIcon />Version history</Button>
+          <br />
+          last modified by <span className="bold-text" style={{ color: 'green' }}>Chandra</span>
+          <br />
+          15 Apr 2019 10:00 AM
         </Col>
       </Row>
     </CardBody>

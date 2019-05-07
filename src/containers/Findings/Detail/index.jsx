777b512d0c4887fe53d3de088087f7f28b6a4f32
alfiant <alfiant@gmail.com>
2019-05-07 12:32:23 -0700

    finding detail update

--- src/containers/Findings/Detail/index.jsx
+++ src/containers/Findings/Detail/index.jsx
@@ -2,16 +2,16 @@ import React from 'react';
 import {
   Col, Container, Row, Card, CardBody, Breadcrumb, BreadcrumbItem,
 } from 'reactstrap';
-import HorizontalForm from './components/HorizontalForm';
 
 const ScansDetail = () => (
   <Container>
     <Row>
       <Breadcrumb>
-        <BreadcrumbItem><a href="/engagements/detail">Annual Sale Launch Engagement</a></BreadcrumbItem>
-        <BreadcrumbItem><a href="/appsmodules/detail">Sonia App</a></BreadcrumbItem>
-        <BreadcrumbItem><a href="/scans/detail">WhiteHat Scan</a></BreadcrumbItem>
-        <BreadcrumbItem active>Finding 1234</BreadcrumbItem>
+        <BreadcrumbItem><a href="/appsmodules">Applications</a></BreadcrumbItem>
+        <BreadcrumbItem><a href="/appsmodules/detail">Partner Mobile App</a></BreadcrumbItem>
+        <BreadcrumbItem><a href="/appsmodules/detailversion">Version 2.14 (latest)</a></BreadcrumbItem>
+        <BreadcrumbItem><a href="/scans/detail">Veracode Scan</a></BreadcrumbItem>
+        <BreadcrumbItem active>Cross-site Scripting</BreadcrumbItem>
       </Breadcrumb>
     </Row>
     <Row>
@@ -25,24 +25,30 @@ const ScansDetail = () => (
         <Card>
           <CardBody>
             <div className="card__title">
-              <h5 className="bold-text">Description</h5>
-            </div>
-            System displays an error message Eg: Unable to update the record.
-            But according to functionality system should update the updated record.
-            This particular defect occurs in version 9.2.
-            This particular defect also found in other modules.
-            Inconsistency of application while reproducing the defect.
-          </CardBody>
-        </Card>
-      </Col>
-      <Col>
-        <Card>
-          <CardBody>
-            <div className="card__title">
               <h5 className="bold-text">Details</h5>
             </div>
+            <hr />
             <Row>
-              <Col><HorizontalForm /></Col>
+              <Col xl="4">
+                <h6 className="bold-text">Assigned:</h6>
+                Bob<p />
+                <h6 className="bold-text">Risk:</h6>
+                54.7<p />
+                <h6 className="bold-text">Scan:</h6>
+                Veracode Scan<p />
+                <h6 className="bold-text">Application:</h6>
+                Partner Mobile App v2.14<p />
+                <h6 className="bold-text">Engagement:</h6>
+                Merger Release<p />
+              </Col>
+              <Col xl="8">
+                <h6 className="bold-text">Description:</h6>
+                System displays an error message Eg: Unable to update the record.
+                But according to functionality system should update the updated record.
+                This particular defect occurs in version 9.2.
+                This particular defect also found in other modules.
+                Inconsistency of application while reproducing the defect.
+              </Col>
             </Row>
           </CardBody>
         </Card>

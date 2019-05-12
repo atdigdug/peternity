8180b303bc96e02fe4cc26efe8c2890436f26982
alfiant <alfiant@gmail.com>
2019-05-12 14:04:51 -0700

    Scanners

--- src/containers/Settings/Connections/Add/index.jsx
+++ src/containers/Settings/Connections/Add/index.jsx
@@ -5,6 +5,35 @@ import {
 
 const logoWhitehat = `${process.env.PUBLIC_URL}/img/whitehat.png`;
 const logoVeracode = `${process.env.PUBLIC_URL}/img/veracode.png`;
+const logoAcunetix = `${process.env.PUBLIC_URL}/img/acunetix.png`;
+const logoMicroFocus = `${process.env.PUBLIC_URL}/img/microfocus.png`;
+const logoTrustwave = `${process.env.PUBLIC_URL}/img/trustwave.png`;
+const logoQualys = `${process.env.PUBLIC_URL}/img/qualys.png`;
+const logoRapid7 = `${process.env.PUBLIC_URL}/img/rapid7.png`;
+const logoPortSwigger = `${process.env.PUBLIC_URL}/img/portswigger.png`;
+const logoNstalker = `${process.env.PUBLIC_URL}/img/nstalker.png`;
+const logoErpScan = `${process.env.PUBLIC_URL}/img/erpscan.png`;
+const logoNsFocus = `${process.env.PUBLIC_URL}/img/nsfocus.png`;
+const logoDataTheorem = `${process.env.PUBLIC_URL}/img/datatheorem.png`;
+const logoOpenVas = `${process.env.PUBLIC_URL}/img/openvas.png`;
+const logoNmap = `${process.env.PUBLIC_URL}/img/nmap.png`;
+const logoTenable = `${process.env.PUBLIC_URL}/img/tenable.png`;
+
+const logoSynopsys = `${process.env.PUBLIC_URL}/img/synopsys.png`;
+const logoVirtualForge = `${process.env.PUBLIC_URL}/img/virtualforge.png`;
+const logoCheckMarx = `${process.env.PUBLIC_URL}/img/checkmarx.png`;
+const logoSiteLock = `${process.env.PUBLIC_URL}/img/sitelock.png`;
+const logoBrakeMan = `${process.env.PUBLIC_URL}/img/brakeman.png`;
+const logoSonarCube = `${process.env.PUBLIC_URL}/img/sonarcube.png`;
+
+const logoContrast = `${process.env.PUBLIC_URL}/img/contrast.png`;
+
+const logoSnyk = `${process.env.PUBLIC_URL}/img/snyk.png`;
+const logoSonaType = `${process.env.PUBLIC_URL}/img/sonatype.png`;
+const logoWhiteSource = `${process.env.PUBLIC_URL}/img/whitesource.png`;
+
+const logoPureSec = `${process.env.PUBLIC_URL}/img/puresec.png`;
+const logoProtego = `${process.env.PUBLIC_URL}/img/protego.png`;
 
 const logostyle = {
   left: 0,
@@ -37,8 +66,37 @@ const ScansDetail = () => (
             <div className="card__title">
               <h5 className="bold-text">Scanners</h5>
             </div>
-            <img style={logostyle} src={logoWhitehat} alt="" />
-            <img style={logostyle} src={logoVeracode} alt="" />
+            <img style={logostyle} src={logoWhitehat} alt="" />&nbsp;
+            <img style={logostyle} src={logoVeracode} alt="" />&nbsp;
+            <img style={logostyle} src={logoAcunetix} alt="" />&nbsp;
+            <img style={logostyle} src={logoMicroFocus} alt="" />&nbsp;
+            <img style={logostyle} src={logoTrustwave} alt="" />&nbsp;
+            <img style={logostyle} src={logoQualys} alt="" />&nbsp;
+            <img style={logostyle} src={logoRapid7} alt="" />&nbsp;
+            <img style={logostyle} src={logoPortSwigger} alt="" />&nbsp;
+            <img style={logostyle} src={logoNstalker} alt="" />&nbsp;
+            <img style={logostyle} src={logoErpScan} alt="" />&nbsp;
+            <img style={logostyle} src={logoNsFocus} alt="" />&nbsp;
+            <img style={logostyle} src={logoDataTheorem} alt="" />&nbsp;
+            <img style={logostyle} src={logoOpenVas} alt="" />&nbsp;
+            <img style={logostyle} src={logoNmap} alt="" />&nbsp;
+            <img style={logostyle} src={logoTenable} alt="" />&nbsp;
+            <p />
+            <img style={logostyle} src={logoSynopsys} alt="" />&nbsp;
+            <img style={logostyle} src={logoVirtualForge} alt="" />&nbsp;
+            <img style={logostyle} src={logoCheckMarx} alt="" />&nbsp;
+            <img style={logostyle} src={logoSiteLock} alt="" />&nbsp;
+            <img style={logostyle} src={logoBrakeMan} alt="" />&nbsp;
+            <img style={logostyle} src={logoSonarCube} alt="" />&nbsp;
+            <p />
+            <img style={logostyle} src={logoContrast} alt="" />&nbsp;
+            <p />
+            <img style={logostyle} src={logoSnyk} alt="" />&nbsp;
+            <img style={logostyle} src={logoSonaType} alt="" />&nbsp;
+            <img style={logostyle} src={logoWhiteSource} alt="" />&nbsp;
+            <p />
+            <img style={logostyle} src={logoPureSec} alt="" />&nbsp;
+            <img style={logostyle} src={logoProtego} alt="" />&nbsp;
           </CardBody>
         </Card>
       </Col>

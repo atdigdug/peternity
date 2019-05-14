b71bded553796ca53edee674dd8244335a243580
alfiant <alfiant@gmail.com>
2019-05-14 14:50:03 -0700

    CISO report update

--- src/containers/Reports/Ciso/components/FailureChart.jsx
+++ src/containers/Reports/Ciso/components/FailureChart.jsx
@@ -6,10 +6,10 @@ import {
 import PropTypes from 'prop-types';
 import Panel from '../../../../shared/components/Panel';
 
-const data01 = [{ name: 'Chrome', value: 12934, fill: '#4ce1b6' },
-  { name: 'Safari', value: 9934, fill: '#70bbfd' },
-  { name: 'Mozilla', value: 20432, fill: '#f6da6e' },
-  { name: 'IE', value: 15432, fill: '#ff4861' }];
+const data01 = [{ name: 'Online', value: 12934, fill: '#4ce1b6' },
+  { name: 'Professional', value: 9934, fill: '#70bbfd' },
+  { name: 'Internal', value: 20432, fill: '#f6da6e' },
+  { name: 'Others', value: 15432, fill: '#ff4861' }];
 
 const style = {
   left: 0,
@@ -40,11 +40,11 @@ const VisitorsSessions = () => (
     xl={6}
     md={12}
     title="Failed Engagements"
-    subhead="What browsers are most popular"
+    subhead="By Business Unit"
   >
     <div className="dashboard__visitors-chart">
-      <p className="dashboard__visitors-chart-title">Total visitors <span>on 23.08.2017</span></p>
-      <p className="dashboard__visitors-chart-number">12,384</p>
+      <p className="dashboard__visitors-chart-title">Last Year</p>
+      <p className="dashboard__visitors-chart-number">34</p>
       <ResponsiveContainer className="dashboard__chart-pie" width="100%" height={220}>
         <PieChart className="dashboard__chart-pie-container">
           <Tooltip />

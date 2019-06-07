b764b4e488bd23732d9672adbaa184f00bf9cecb
alfiant <alfiant@gmail.com>
2019-06-07 11:10:20 -0700

    Move pipelines left nav

--- src/containers/Layout/sidebar/SidebarContent.jsx
+++ src/containers/Layout/sidebar/SidebarContent.jsx
@@ -29,13 +29,13 @@ class SidebarContent extends Component {
             <SidebarLink title="Dev Team" route="/reports/devteam" onClick={this.hideSidebar} />
             <SidebarLink title="External" route="/reports/external" onClick={this.hideSidebar} />
           </SidebarCategory>
+          <SidebarLink title="Pipelines" icon="list" route="/pipelines" onClick={this.hideSidebar} />
           <SidebarLink title="Engagements" icon="arrow-up-circle" route="/engagements" onClick={this.hideSidebar} />
           <SidebarLink title="Apps/Modules" icon="layers" route="/appsmodules" onClick={this.hideSidebar} />
           <SidebarLink title="Scans" icon="magnifier" route="/scans" onClick={this.hideSidebar} />
           <SidebarLink title="Findings" icon="bug" route="/findings" onClick={this.hideSidebar} />
           <SidebarLink title="Risks" icon="warning" route="/findings/riskanalysis" onClick={this.hideSidebar} />
           <SidebarLink title="Dev Support" icon="code" route="/devsupport" onClick={this.hideSidebar} />
-          <SidebarLink title="Pipelines" icon="list" route="/pipelines" onClick={this.hideSidebar} />
         </ul>
         <ul className="sidebar__block">
           <SidebarCategory title="Settings" icon="cog">

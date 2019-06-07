a81f1ed1121e9f3e86fd90db4a95ca5792b9030f
alfiant <alfiant@gmail.com>
2019-06-07 11:52:12 -0700

    Pipeline detail - add running alert

--- src/containers/Pipelines/Detail/index.jsx
+++ src/containers/Pipelines/Detail/index.jsx
@@ -3,6 +3,7 @@ import {
   Col, Container, Row, Card, CardBody, Breadcrumb, BreadcrumbItem,
 } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import Alert from '../../AsfShared/ScanAlert';
 import ColumnarData from './components/Columnar';
 import Timeline from './components/TimelineHistory';
 
@@ -17,12 +18,19 @@ const PipelineDetail = () => (
     <Row>
       <Col md={12}>
         <h3 className="page-title">Pipeline : Jenkins 2946</h3>
-        <h3 className="page-subhead subhead">
-          Running 1
-        </h3>
       </Col>
     </Row>
+    <Row>
+      <Col>
+        <Alert color="success" className="alert--colored" icon>
+          <p>1 scan running...</p>
+        </Alert>
+      </Col>
+    </Row>
+    <p />
+
     <ColumnarData />
+
     <div className="card__title">
       <h5 className="bold-text">Timeline</h5>
     </div>

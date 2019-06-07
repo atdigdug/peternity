310bbf7cb5552e11dbe0ad71dc62eda1aa0f1315
alfiant <alfiant@gmail.com>
2019-06-07 11:29:16 -0700

    Pipelines - condensed/detailed

--- src/containers/Pipelines/components/StatusTable.jsx
+++ src/containers/Pipelines/components/StatusTable.jsx
@@ -1,89 +1,93 @@
 import React from 'react';
 import {
-  Table, Card, CardBody,
+  Table, Card, CardBody, Row, Col,
 } from 'reactstrap';
 
 const EngagementDataTable = () => (
-  <Card>
-    <CardBody>
-      <Table responsive hover className="table--bordered dashboard__audience-table">
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Source</th>
-            <th>Status</th>
-            <th>App</th>
-            <th>Tool</th>
-            <th>Sken Suggestion</th>
-            <th>Finding</th>
-            <th>Time Taken</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>1</td>
-            <td>Jenkins 2946</td>
-            <td>Pending</td>
-            <td>Partner Mobile App</td>
-            <td>FxCop</td>
-            <td>-</td>
-            <td>-</td>
-            <td>-</td>
-          </tr>
-          <tr>
-            <td>2</td>
-            <td>Jenkins 2946</td>
-            <td>Running</td>
-            <td>Partner Mobile App</td>
-            <td>Veracode</td>
-            <td>-</td>
-            <td>-</td>
-            <td>-</td>
-          </tr>
-          <tr>
-            <td>3</td>
-            <td>Jenkins 2946</td>
-            <td>Completed</td>
-            <td>Partner Mobile App</td>
-            <td>WhiteHat</td>
-            <td>OK</td>
-            <td>325</td>
-            <td>2:43</td>
-          </tr>
-          <tr>
-            <td>4</td>
-            <td>Jenkins 2946</td>
-            <td>Error</td>
-            <td>Partner Mobile App</td>
-            <td>CheckMarx</td>
-            <td>Fix Error</td>
-            <td>-</td>
-            <td>-</td>
-          </tr>
-          <tr>
-            <td>5</td>
-            <td>Jenkins 9455</td>
-            <td>Completed</td>
-            <td>Customer Portal</td>
-            <td>Veracode</td>
-            <td>Investigate</td>
-            <td>258</td>
-            <td>3:29</td>
-          </tr>
-          <tr>
-            <td>6</td>
-            <td>Jenkins 9156</td>
-            <td>Completed</td>
-            <td>Vendor Registration App</td>
-            <td>FxCop</td>
-            <td>Remediate</td>
-            <td>694</td>
-            <td>1:01</td>
-          </tr>
-        </tbody>
-      </Table>
-    </CardBody>
-  </Card>
+  <Row>
+    <Col>
+      <Card>
+        <CardBody>
+          <Table responsive hover className="table--bordered dashboard__audience-table">
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Source</th>
+                <th>Status</th>
+                <th>App</th>
+                <th>Tool</th>
+                <th>Sken Suggestion</th>
+                <th>Finding</th>
+                <th>Time Taken</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>1</td>
+                <td>Jenkins 2946</td>
+                <td>Pending</td>
+                <td>Partner Mobile App</td>
+                <td>FxCop</td>
+                <td>-</td>
+                <td>-</td>
+                <td>-</td>
+              </tr>
+              <tr>
+                <td>2</td>
+                <td>Jenkins 2946</td>
+                <td>Running</td>
+                <td>Partner Mobile App</td>
+                <td>Veracode</td>
+                <td>-</td>
+                <td>-</td>
+                <td>-</td>
+              </tr>
+              <tr>
+                <td>3</td>
+                <td>Jenkins 2946</td>
+                <td>Completed</td>
+                <td>Partner Mobile App</td>
+                <td>WhiteHat</td>
+                <td>OK</td>
+                <td>325</td>
+                <td>2:43</td>
+              </tr>
+              <tr>
+                <td>4</td>
+                <td>Jenkins 2946</td>
+                <td>Error</td>
+                <td>Partner Mobile App</td>
+                <td>CheckMarx</td>
+                <td>Fix Error</td>
+                <td>-</td>
+                <td>-</td>
+              </tr>
+              <tr>
+                <td>5</td>
+                <td>Jenkins 9455</td>
+                <td>Completed</td>
+                <td>Customer Portal</td>
+                <td>Veracode</td>
+                <td>Investigate</td>
+                <td>258</td>
+                <td>3:29</td>
+              </tr>
+              <tr>
+                <td>6</td>
+                <td>Jenkins 9156</td>
+                <td>Completed</td>
+                <td>Vendor Registration App</td>
+                <td>FxCop</td>
+                <td>Remediate</td>
+                <td>694</td>
+                <td>1:01</td>
+              </tr>
+            </tbody>
+          </Table>
+        </CardBody>
+      </Card>
+    </Col>
+  </Row>
 );
 
 export default EngagementDataTable;
--- src/containers/Pipelines/index.jsx
+++ src/containers/Pipelines/index.jsx
@@ -1,52 +1,85 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import {
-  Col, Container, Row, Card, CardBody,
+  Col, Container, Row, Card, CardBody, UncontrolledDropdown, DropdownToggle, DropdownMenu,
+  DropdownItem, ButtonGroup, Button,
 } from 'reactstrap';
+import ChevronDownIcon from 'mdi-react/ChevronDownIcon';
+import CondensedIcon from 'mdi-react/MenuIcon';
+import DetailedIcon from 'mdi-react/ViewAgendaIcon';
 import Aggregate from '../AsfShared/GenericAggregate';
 import StatusTable from './components/StatusTable';
 import ColumnarData from './components/Columnar';
 import Timeline from './components/TimelineHistory';
 
-const ScansDetail = () => (
-  <Container>
-    <Row>
-      <Col md={12}>
-        <h3 className="page-title">Pipelines</h3>
-        <h3 className="page-subhead subhead">
-          Running 3
-        </h3>
-      </Col>
-    </Row>
-    <Row><Aggregate /></Row>
-    <div className="card__title">
-      <h5 className="bold-text">Tabular</h5>
-    </div>
-    <Row>
-      <Col>
-        <Card>
-          <CardBody>
-            <StatusTable />
-          </CardBody>
-        </Card>
-      </Col>
-    </Row>
-    <div className="card__title">
-      <h5 className="bold-text">Columnar</h5>
-    </div>
-    <ColumnarData />
-    <div className="card__title">
-      <h5 className="bold-text">Timeline : Jenkins 2946</h5>
-    </div>
-    <Row>
-      <Col>
-        <Card>
-          <CardBody>
-            <Timeline />
-          </CardBody>
-        </Card>
-      </Col>
-    </Row>
-  </Container>
-);
+export default class ScansDetail extends PureComponent {
+  constructor() {
+    super();
+    this.state = { isCondensed: true };
+  }
 
-export default ScansDetail;
+  render() {
+    let dataTable;
+
+    document.title = 'Engagements - AppSecFlo';
+
+    if (this.state.isCondensed) {
+      dataTable = <StatusTable />;
+    } else {
+      dataTable = <ColumnarData />;
+    }
+
+    return (
+      <Container>
+        <Row>
+          <Col md={12}>
+            <h3 className="page-title">Pipelines</h3>
+            <h3 className="page-subhead subhead">
+              Running 3
+            </h3>
+          </Col>
+        </Row>
+        <Row><Aggregate /></Row>
+        <Row>
+          <Col xl="6">
+            <UncontrolledDropdown>
+              <DropdownToggle className="icon icon--right">
+                <p>Most urgent <ChevronDownIcon /></p>
+              </DropdownToggle>
+              <DropdownMenu className="dropdown__menu">
+                <DropdownItem>Most urgent</DropdownItem>
+                <DropdownItem>Most recent</DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+          </Col>
+          <Col xl="6">
+            <div className="float-right">
+              <ButtonGroup>
+                <Button className="icon" onClick={() => this.setState({ isCondensed: true })}>
+                  <p><CondensedIcon /> Condensed</p>
+                </Button>
+                <Button className="icon" outline onClick={() => this.setState({ isCondensed: false })}>
+                  <p><DetailedIcon /> Detailed</p>
+                </Button>
+              </ButtonGroup>
+            </div>
+          </Col>
+        </Row>
+
+        {dataTable}
+
+        <div className="card__title">
+          <h5 className="bold-text">Timeline : Jenkins 2946</h5>
+        </div>
+        <Row>
+          <Col>
+            <Card>
+              <CardBody>
+                <Timeline />
+              </CardBody>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+}

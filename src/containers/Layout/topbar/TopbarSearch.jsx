4fad93b5a176b1243d4d8f596481e9516d8f4359
alfiant <alfiant@gmail.com>
2019-07-10 14:41:45 -0700

    Auto complete for search

--- src/containers/Layout/topbar/TopbarSearch.jsx
+++ src/containers/Layout/topbar/TopbarSearch.jsx
@@ -1,6 +1,6 @@
 import React, { PureComponent } from 'react';
 import SearchIcon from 'mdi-react/SearchIcon';
-import classNames from 'classnames';
+import Autocomplete from 'react-autocomplete';
 
 export default class TopbarSearch extends PureComponent {
   constructor() {
@@ -14,20 +14,30 @@ export default class TopbarSearch extends PureComponent {
   };
 
   render() {
-    const { inputOpen } = this.state;
-
-    const searchClass = classNames({
-      'topbar__search-field': true,
-      'topbar__search-field--open': inputOpen,
-    });
-
     return (
-      <form className="topbar__search" action="/searchresults">
-        <input placeholder="search..." className={searchClass} />
-        <button className="topbar__btn topbar__search-btn" type="button" onClick={this.onInputOpen}>
+      <div>
+        <br />
+        <form className="topbar__search" action="/searchresults">
+          <Autocomplete
+            items={[
+              { id: 'foo', label: 'invoice template' },
+              { id: 'bar', label: 'involvement' },
+              { id: 'baz', label: 'invite' },
+            ]}
+            shouldItemRender={(item, value) => item.label.toLowerCase().indexOf(value.toLowerCase()) > -1}
+            getItemValue={item => item.label}
+            renderItem={(item, highlighted) => (
+              <div key={item.id} style={{ backgroundColor: highlighted ? '#eee' : 'transparent' }}>
+                {item.label}
+              </div>
+            )}
+            value={this.state.value}
+            onChange={e => this.setState({ value: e.target.value })}
+            onSelect={value => this.setState({ value })}
+          />
           <SearchIcon />
-        </button>
-      </form>
+        </form>
+      </div>
     );
   }
 }
